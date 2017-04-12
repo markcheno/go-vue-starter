04ea75c9ea93bc7b7be3e0aa8d8887524edc907f
Mark Chenoweth <mark@Marks-MacBook-Pro.local>
2017-04-11 20:46:41 -0500

    verbage

--- client/src/components/SecretQuote.vue
+++ client/src/components/SecretQuote.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-sm-6 col-sm-offset-3">
-    <h1>Get a protected quote!</h1>
+    <h1>Get a secret quote!</h1>
     <button class="btn btn-warning" v-on:click="getQuote()">Get a Quote</button>
     <div class="quote-area" v-if="quote">
       <h2><blockquote>{{ quote }}</blockquote></h2>

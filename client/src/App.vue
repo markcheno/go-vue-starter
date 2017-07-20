9e993f208b403793a54b6fcd100bf6aaef55d5c3
Mark Chenoweth <mark@Marks-MacBook-Pro.local>
2017-07-19 21:27:50 -0500

    modified header bar, added jquery/bootstrap.js

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,30 @@
 <template>
 <div id="app">
-  <nav class="navbar navbar-default">
+
+  <nav class="navbar navbar-default navbar-static-top">
     <div class="container">
-      <ul class="nav navbar-nav">
-        <li><router-link to="home">Home</router-link></li>
-        <li v-if="!isAuthenticated()"><router-link to="login">Login</router-link></li>
-        <li v-if="!isAuthenticated()"><router-link to="signup">Sign Up</router-link></li>
-        <li v-if="isAuthenticated()"><router-link to="secretquote">Secret Quote</router-link></li>
-        <li v-if="isAuthenticated()"><router-link to="userinfo">User Info</router-link></li>
-        <li v-if="isAuthenticated()"><router-link to="logout" @click.native="logout()">Logout</router-link></li>
-      </ul>
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Brand</a>
+      </div>
+      <div id="navbar" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+          <li><router-link to="home">Home</router-link></li>
+          <li v-if="!isAuthenticated()"><router-link to="login">Login</router-link></li>
+          <li v-if="!isAuthenticated()"><router-link to="signup">Sign Up</router-link></li>
+          <li v-if="isAuthenticated()"><router-link to="secretquote">Secret Quote</router-link></li>
+          <li v-if="isAuthenticated()"><router-link to="userinfo">User Info</router-link></li>
+          <li v-if="isAuthenticated()"><router-link to="logout" @click.native="logout()">Logout</router-link></li>
+        </ul>
+      </div>
     </div>
   </nav>
+
   <div class="container">
     <router-view></router-view>
   </div>
